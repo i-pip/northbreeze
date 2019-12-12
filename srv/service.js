a42910e083da3ac6eefb85e236816424d60e2d47
pip <jrphiri@gmail.com>
2019-12-12 15:10:11 +0200

    Removed unnecessary console.log

--- srv/service.js
+++ srv/service.js
@@ -2,7 +2,7 @@ module.exports = srv => {
   if (srv.name === "Breezy") {
     srv.on("hello", x => `hello there ${x.data.to} !`);
     srv.before("READ", "Products", x => {
-      console.log(x);
+      // console.log(x);
     });
   }
 };
